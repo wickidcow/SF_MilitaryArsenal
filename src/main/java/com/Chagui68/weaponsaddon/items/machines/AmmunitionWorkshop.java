2114fe17698e127d06d145e65f4d6128ae66ffa0
wickidcow <jaywago@gmail.com>
2026-08-23 17:02:41 -0400

    Fix duplicate Slimefun registration for ammunition workshop

--- src/main/java/com/Chagui68/weaponsaddon/items/machines/AmmunitionWorkshop.java
+++ src/main/java/com/Chagui68/weaponsaddon/items/machines/AmmunitionWorkshop.java
@@ -6,13 +6,10 @@ import com.github.drakescraft_labs.slimefun4.api.items.SlimefunItemStack;
 import com.github.drakescraft_labs.slimefun4.api.recipes.RecipeType;
 import com.github.drakescraft_labs.slimefun4.api.SlimefunAddon;
 import com.github.drakescraft_labs.slimefun4.core.handlers.BlockUseHandler;
-import com.github.drakescraft_labs.slimefun4.core.handlers.BlockPlaceHandler;
 import com.github.drakescraft_labs.slimefun4.implementation.SlimefunItems;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class AmmunitionWorkshop extends SlimefunItem {
@@ -33,14 +30,6 @@ public class AmmunitionWorkshop extends SlimefunItem {
 
     @Override
     public void preRegister() {
-        addItemHandler(new BlockPlaceHandler(false) {
-            @Override
-            public void onPlayerPlace(BlockPlaceEvent e) {
-                Block b = e.getBlock();
-                BlockStorage.addBlockInfo(b, "id", "MA_AMMUNITION_WORKSHOP");
-            }
-        });
-
         addItemHandler((BlockUseHandler) e -> {
             e.cancel();
             Player p = e.getPlayer();
